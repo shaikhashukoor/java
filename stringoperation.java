7f49e6a3423cd1b3ffaecd98ac5ceebd2348e1b6
shaikhashukoor <kkshaikha94@gmail.com>
2026-02-18 11:11:54 +0530

    Add files via upload

---
+++ stringoperation.java
@@ -0,0 +1,14 @@
+import util.java.Scanner;
+public class stringoperation{
+public static void main (String [] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a string");
+string s1=sc.nextLine();
+System.out.println("the length of the string is "+ s1.length());
+}
+}
+
+
+
+
+
